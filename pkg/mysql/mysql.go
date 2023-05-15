4839e122b8bb2c38364a9d5cbc8f8d37b2ccebe8
@irarmdhnty11 <irarmdhnty11@gmail.com>
2023-05-15 12:29:20 +0700

    first

--- pkg/mysql/mysql.go
+++ pkg/mysql/mysql.go
@@ -12,7 +12,7 @@ var DB *gorm.DB
 func DatabaseInit() {
 	var err error
 
-	dsn := "root:@tcp(localhost:3306)/qurban?charset=utf8mb4,utf8&sys_var=esc%40ped"
+	dsn := "irarmdhnty23:Admin123@tcp(db4free.net:3306)/qurban"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
